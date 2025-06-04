d9be08757072e9e8803c5ee065a0b10509ac33e9
SamiChafik <sami.chafik00@gmail.com>
2025-06-04 23:51:30 +0100

    getting token from the api after login and storing it locally

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,20 +1,56 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders  } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:8080'; // Replace with your API URL
+  private apiUrl = 'http://localhost:8080/api/auth';
+  private tokenKey = 'auth_token';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   login(credentials: { email: string, password: string }): Observable<any> {
-    return this.http.post(`${this.apiUrl}/api/auth/authenticate`, credentials);
+    return this.http.post(`${this.apiUrl}/authenticate`, credentials).pipe(
+      tap((response: any) => {
+        if (response.token) {
+          this.setToken(response.token);
+          console.log(response.token);
+        }
+      })
+    );
   }
 
   register(userData: { name: string, email: string, password: string, role: string }): Observable<any> {
-    return this.http.post(`${this.apiUrl}/api/auth/register`, userData);
+    return this.http.post(`${this.apiUrl}/register`, userData);
+  }
+
+  setToken(token: string): void {
+    localStorage.setItem(this.tokenKey, token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.tokenKey);
+  }
+
+  removeToken(): void {
+    localStorage.removeItem(this.tokenKey);
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.getToken();
+  }
+
+  getAuthenticatedUser(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/user`, {
+      headers: this.getAuthHeaders()
+    });
+  }
+
+  getAuthHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Authorization': `Bearer ${this.getToken()}`
+    });
   }
 }
