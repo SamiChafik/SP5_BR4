e5fd11e893e288e0fe77cbe24bfd8643d5491133
SamiChafik <sami.chafik00@gmail.com>
2025-06-04 23:49:57 +0100

    disabled role select (testing)

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -44,7 +44,7 @@ export class RegisterComponent {
     this.registerForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
+      password: ['', [Validators.required, Validators.minLength(3)]],
       role: ['CLIENT', Validators.required]
     });
   }
