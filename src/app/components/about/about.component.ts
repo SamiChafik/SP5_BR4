cc77ef7b66611d9f1d8e2b3dad2cc71ca8dd51c8
SamiChafik <sami.chafik00@gmail.com>
2025-06-06 22:18:03 +0100

    about component

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatIconModule,
+    MatListModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  email = 'hello@helloevents.com';
 
 }
