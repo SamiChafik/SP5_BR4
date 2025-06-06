04296810a0d54bab177d72a31acddd9fc5d88801
SamiChafik <sami.chafik00@gmail.com>
2025-06-06 22:24:05 +0100

    path to users list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { RegisterComponent } from './components/register/register.component';
 import { EventsComponent } from './components/events/events.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
+import { UserListComponent } from './components/user-list/user-list.component';
 
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'events', component: EventsComponent },
+  { path: 'users', component: UserListComponent},
   // { path: 'reservations', component: ReservationsComponent },
   // { path: 'dashboard', component: DashboardComponent },
   { path: 'login', component: LoginComponent },
