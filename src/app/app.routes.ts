2f108311ab9e138c77793c918437dd3398a15811
SamiChafik <sami.chafik00@gmail.com>
2025-06-08 02:17:37 +0100

    added new route + protected some routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,17 @@ import { EventsComponent } from './components/events/events.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { UserListComponent } from './components/user-list/user-list.component';
+import { AuthGuard } from './guard/auth.guard';
+import { AdminGuard } from './guard/admin.guard';
+import { BookingComponent } from './components/booking/booking.component';
 
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'events', component: EventsComponent },
-  { path: 'users', component: UserListComponent},
+  { path: 'users', component: UserListComponent, canActivate:[AuthGuard, AdminGuard]},
+  { path: 'bookings', component: BookingComponent, canActivate:[AuthGuard]},
   // { path: 'reservations', component: ReservationsComponent },
   // { path: 'dashboard', component: DashboardComponent },
   { path: 'login', component: LoginComponent },
