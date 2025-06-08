bb28a12ccd318a46ba0833ad78c842babda92eb4
SamiChafik <sami.chafik00@gmail.com>
2025-06-08 02:14:16 +0100

    added auth verification + booking

--- src/app/components/events/events.component.ts
+++ src/app/components/events/events.component.ts
@@ -9,6 +9,8 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { EventService } from '../../services/events.service';
 import { BookingService } from '../../services/booking.service';
 import { AuthService } from '../../services/auth.service';
+import { MatDialog } from '@angular/material/dialog';
+import { EventFormComponent } from '../event-form/event-form.component';
 
 type Event = {
   event_id: number;
@@ -35,16 +37,26 @@ type Event = {
 export class EventsComponent implements OnInit {
   events: Event[] = [];
   isLoading = true;
+  currentUser: any = null;
 
   constructor(
     private eventsService: EventService,
     private bookingService: BookingService,
     private snackBar: MatSnackBar,
-    private authService: AuthService
+    public authService: AuthService,
+    private dialog: MatDialog
   ) { }
 
   ngOnInit(): void {
     this.loadEvents();
+
+    this.currentUser = this.authService.getUser();
+    
+    if (this.authService.userUpdated$) {
+      this.authService.userUpdated$.subscribe(() => {
+        this.currentUser = this.authService.getUser();
+      });
+    }
   }
 
   loadEvents(): void {
@@ -68,12 +80,37 @@ export class EventsComponent implements OnInit {
     return;
   }
 
-  this.bookingService.createBooking(event.event_id).subscribe({
-    next: () => {
-      this.snackBar.open(`Successfully booked ${event.eventname}`, 'Close', { duration: 3000 });
-    },
-    error: (err) => {
-      this.snackBar.open(err.message || 'Booking failed', 'Close', { duration: 3000 });
+    this.bookingService.createBooking(event.event_id).subscribe({
+      next: () => {
+        this.snackBar.open(`Successfully booked ${event.eventname}`, 'Close', { duration: 3000 });
+      },
+      error: (err) => {
+        this.snackBar.open(err.message || 'Booking failed', 'Close', { duration: 3000 });
+      }
+    });
+  }
+
+  // openCreateDialog(): void {
+  //   const dialogRef = this.dialog.open(EventFormComponent, {
+  //     width: '600px'
+  //   });
+
+  //   dialogRef.afterClosed().subscribe(result => {
+  //     if (result) {
+  //       this.loadEvents();
+  //     }
+  //   });
+  // }
+
+openEditDialog(event: any): void {
+  const dialogRef = this.dialog.open(EventFormComponent, {
+    width: '600px',
+    data: { event }
+  });
+
+  dialogRef.afterClosed().subscribe(result => {
+    if (result) {
+      this.loadEvents();
     }
   });
 }
